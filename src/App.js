7c5b7d13fc79ff69dbd420358fca878565877cbd
Daniel FM Furlan <daniel@debianrock.rock>
2020-05-21 15:15:55 -0300

    uhm...

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         # Edit <code>src/App.js</code> and save to reload.
-           Our continous Deployment works!!
+           Our continous Deployment works!! Are you sure?
         </p>
         <a
           className="App-link"
