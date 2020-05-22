7c751a7db9fd2e26c15624abd79d3ea12557d8b8
Daniel FM Furlan <daniel@debianrock.rock>
2020-05-22 10:33:05 -0300

    ash

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <!--Edit <code>src/App.js</code> and save to reload.
-           --!>
+         
            Our continous Deployment works!! Are you sure?
         </p>
         <a
